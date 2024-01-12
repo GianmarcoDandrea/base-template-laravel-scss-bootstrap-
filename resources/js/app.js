c58cb75d07e9e5f0e86f8ac9708fe15813975ada
GianmarcoDandrea <gianm96@hotmail.it>
2024-01-12 12:24:36 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import "./bootstrap";
+
+import "~resources/scss/app.scss";
+// Per permettere a vite di processare le immagini
+import.meta.glob(["../img/**"]);
+
+// Importiamo parte js di bootstrap css
+import * as bootstrap from 'bootstrap';
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Welcome to laravel</h1>
+@endsection
