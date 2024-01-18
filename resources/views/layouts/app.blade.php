f77a5089dc5d1142c86f8e2d0d5993b29aef8203
Gianmarco D'Andrea <142946273+GianmarcoDandrea@users.noreply.github.com>
2024-01-18 18:35:36 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,10 +10,12 @@
     @vite('resources/js/app.js')
 </head>
 <body>
+    @include('partials.header')
 
     <main>
         @yield('content')
     </main>
-    
+
+    @include('partials.footer')
 </body>
 </html>
